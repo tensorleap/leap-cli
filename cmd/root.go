39e3a37e405b54050956736f07335bb4e504dacb
Arik Maor <arik.maor@tensorleap.ai>
2023-05-21 14:50:41 +0300

    Install viper

--- cmd/root.go
+++ cmd/root.go
@@ -3,8 +3,10 @@ package cmd
 import (
 	"fmt"
 	"os"
+  "path"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"github.com/tensorleap/cli-go/cmd/auth"
 	"github.com/tensorleap/cli-go/cmd/datasets"
 	"github.com/tensorleap/cli-go/cmd/local"
@@ -18,7 +20,11 @@ var RootCommand = &cobra.Command{
 Complete documentation is available at http://docs.tensoleap.ai`,
 }
 
+var cfgFile string
+
 func init() {
+  cobra.OnInitialize(initConfig)
+	RootCommand.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.config/tensorleap/config.yaml)")
 	RootCommand.AddCommand(auth.RootCommand)
 	RootCommand.AddCommand(local.RootCommand)
   RootCommand.AddCommand(datasets.RootCommand)
@@ -31,3 +37,24 @@ func Execute() {
 		os.Exit(1)
 	}
 }
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		homeDir, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+    configDir := path.Join(homeDir, ".config", "tensorleap")
+		viper.AddConfigPath(configDir)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName("config")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
+
