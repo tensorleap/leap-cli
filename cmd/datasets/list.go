d0c3296759386dd49a2dc1d49796ebe39e54f9f4
Arik Maor <arik.maor@tensorleap.ai>
2023-05-23 02:02:15 +0300

    Implement datasets list

--- cmd/datasets/list.go
+++ cmd/datasets/list.go
@@ -2,7 +2,10 @@ package datasets
 
 import (
 	"fmt"
+
 	"github.com/spf13/cobra"
+	. "github.com/tensorleap/cli-go/pkg/api"
+	"github.com/tensorleap/cli-go/pkg/config"
 )
 
 func init() {
@@ -11,7 +14,12 @@ func init() {
     Short: "List available datasets",
     Long:  `List available datasets`,
     Run: func(cmd *cobra.Command, args []string) {
-      fmt.Println("List command")
+      config.VerifyLoggedIn()
+      data, _, err := ApiClient.GetDatasets(cmd.Context()).Execute()
+      cobra.CheckErr(err)
+      for _, dataset := range data.Datasets {
+        fmt.Println(dataset.GetId(), "-",  dataset.GetName())
+      }
     },
   })
 }
