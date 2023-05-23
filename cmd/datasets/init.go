0db118d53f1381a07f3105abf10e9858a92fafa4
Arik Maor <arik.maor@tensorleap.ai>
2023-05-23 13:50:37 +0300

    Add init stub

---
+++ cmd/datasets/init.go
@@ -0,0 +1,17 @@
+package datasets
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+  RootCommand.AddCommand(&cobra.Command{
+    Use:   "save",
+    Short: "Save dataset script",
+    Long:  `Save dataset script`,
+    Run: func(cmd *cobra.Command, args []string) {
+      fmt.Println("Save command")
+    },
+  })
+}
