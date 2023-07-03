bf595aaa2a7d1b009831c35050f2d7cb4ce02266
Moshe Kabala <moshe.kabala@tensorleap.ai>
2023-07-03 17:00:01 +0300

    Support data volume

--- pkg/helm/helm_test.go
+++ pkg/helm/helm_test.go
@@ -7,16 +7,17 @@ import (
 )
 
 func TestCreateTensorleapChartValues(t *testing.T) {
-	t.Run("CreateTensorleapChartValues with useGpu", func(t *testing.T) {
-
+	t.Run("CreateTensorleapChartValues", func(t *testing.T) {
 		useGpu := true
+		dataDir := "some/dir/path"
 		expected := Record{
 			"tensorleap-engine": Record{
-				"gpu": useGpu,
+				"gpu":                useGpu,
+				"localDataDirectory": dataDir,
 			},
 		}
 
-		result := CreateTensorleapChartValues(useGpu)
+		result := CreateTensorleapChartValues(useGpu, dataDir)
 
 		assert.Equal(t, expected, result)
 	})
