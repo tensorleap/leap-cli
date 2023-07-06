0dcb8b856bc1d3a675283bea4afa8f649e1bc47a
Moshe Kabala <moshe.kabala@tensorleap.ai>
2023-07-06 15:19:16 +0300

    Extract create helm config from the helm install

--- pkg/helm/install.go
+++ pkg/helm/install.go
@@ -3,38 +3,27 @@ package helm
 import (
 	"context"
 	"log"
-	"os"
 
 	"helm.sh/helm/v3/pkg/action"
 	"helm.sh/helm/v3/pkg/chart/loader"
-	"helm.sh/helm/v3/pkg/cli"
 )
 
 func InstallLatestTensorleapChartVersion(
 	ctx context.Context,
-	kubeContext string,
-	namespace string,
+	config *HelmConfig,
 	values Record,
 ) error {
-	settings := cli.New()
-	settings.SetNamespace(namespace)
-	settings.KubeContext = kubeContext
+	
 
-	actionConfig := new(action.Configuration)
-	if err := actionConfig.Init(settings.RESTClientGetter(), settings.Namespace(), os.Getenv("HELM_DRIVER"), log.Printf); err != nil {
-		log.Printf("%+v", err)
-		os.Exit(1)
-	}
-
-	client := action.NewInstall(actionConfig)
+	client := action.NewInstall(config.ActionConfig)
 	client.ChartPathOptions.RepoURL = REPO_URL
-	client.Namespace = settings.Namespace()
+	client.Namespace = config.Namespace
 	client.CreateNamespace = true
 	client.Wait = true
 	client.ReleaseName = RELEASE_NAME
 
 	// client := action.NewPull()
-	cp, err := client.ChartPathOptions.LocateChart(CHART_NAME, settings)
+	cp, err := client.ChartPathOptions.LocateChart(CHART_NAME, config.Settings)
 	if err != nil {
 		return err
 	}
--- pkg/helm/utils.go
+++ pkg/helm/utils.go
@@ -1,11 +1,19 @@
 package helm
 
+import (
+	"log"
+	"os"
+
+	"helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/cli"
+)
+
 type Record = map[string]interface{}
 
 const (
-	RELEASE_NAME="tensorleap"
-	CHART_NAME="tensorleap"
-	REPO_URL="https://helm.tensorleap.ai"
+	RELEASE_NAME = "tensorleap"
+	CHART_NAME   = "tensorleap"
+	REPO_URL     = "https://helm.tensorleap.ai"
 )
 
 func CreateTensorleapChartValues(useGpu bool, dataDir string) Record {
@@ -15,4 +23,27 @@ func CreateTensorleapChartValues(useGpu bool, dataDir string) Record {
 			"localDataDirectory": dataDir,
 		},
 	}
+}
+
+type HelmConfig struct {
+	Namespace    string
+	ActionConfig *action.Configuration
+	Settings *cli.EnvSettings
+}
+
+func CreateHelmConfig(kubeContext, namespace string) (*HelmConfig, error) {
+	settings := cli.New()
+	settings.SetNamespace(namespace)
+	settings.KubeContext = kubeContext
+
+	actionConfig := new(action.Configuration)
+	if err := actionConfig.Init(settings.RESTClientGetter(), settings.Namespace(), os.Getenv("HELM_DRIVER"), log.Printf); err != nil {
+		return nil, err
+	}
+
+	return &HelmConfig{
+		Namespace:    namespace,
+		ActionConfig: actionConfig,
+		Settings: settings,
+	}, nil
 }
