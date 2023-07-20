cc23f80a67e1b6cf94f5154d977e588d708ed060
Moshe Kabala <moshe.kabala@tensorleap.ai>
2023-07-20 15:10:01 +0300

    Add extract tar and download and extract tar

---
+++ pkg/local/files_test.go
@@ -0,0 +1,30 @@
+package local
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExtractTarGzFile(t *testing.T) {
+	file, _ := os.Open("./test_assets/project.tar.zip")
+	defer file.Close()
+	outputDir := "./exported"
+	defer os.RemoveAll(outputDir)
+	files, err := ExtractTarGzFile(file, outputDir)
+	assert.NoError(t, err)
+	assert.FileExists(t, filepath.Join(outputDir, "project.json"))
+	assert.Len(t, files, 13)
+}
+
+func TestDownloadAndExtractTarFile(t *testing.T) {
+	url := "https://hub.tensorleap.ai/demo/projects/MNIST/versions/81/1686477167629-project.tar"
+	outputDir := "./exported"
+	defer os.RemoveAll(outputDir)
+	files, err := DownloadAndExtractTarFile(url, outputDir)
+	assert.NoError(t, err)
+	assert.FileExists(t, filepath.Join(outputDir, "project.json"))
+	assert.Len(t, files, 42)
+}
