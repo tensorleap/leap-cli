e4c5faacc5da9b2989603091fb177e5bca3fe9ac
Moshe Kabala <moshe.kabala@tensorleap.ai>
2023-08-06 16:03:52 +0300

    Add file checksup function

--- pkg/local/files_test.go
+++ pkg/local/files_test.go
@@ -19,6 +19,17 @@ func TestExtractTarGzFile(t *testing.T) {
 	assert.Len(t, files, 13)
 }
 
+func TestCalculateSHA256(t *testing.T) {
+	file, _ := os.Open("./test_assets/project.tar.zip")
+	defer file.Close()
+
+	hash1, _ := GetFileChecksum(file)
+	_, _ = file.Seek(0, 0)
+	hash2, _ := GetFileChecksum(file)
+
+	assert.Equal(t, hash1, hash2)
+}
+
 func TestDownloadAndExtractTarFile(t *testing.T) {
 	t.Skip("skip test") // fix assets
 	url := "https://hub.tensorleap.ai/demo/projects/MNIST/versions/81/1686477167629-project.tar"
