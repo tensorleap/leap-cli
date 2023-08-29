44d6fa9ad630f76b7042d89516255108b3811c65
Moshe Kabala <moshe.kabala@tensorleap.ai>
2023-08-29 15:03:36 +0300

    Update update command to restart cluser before upgrade

--- pkg/server/upgrade.go
+++ pkg/server/upgrade.go
@@ -14,20 +14,20 @@ func ValidateStandaloneDir() error {
 	_, err := os.Stat(local.STANDALONE_DIR)
 	if os.IsNotExist(err) {
 		log.SendCloudReport("error", "Installation dir not found", "Failed", &map[string]interface{}{"error": err.Error()})
-		return fmt.Errorf("Not found data directory(%s) on this machine, Please make sure to install before upgrade", local.STANDALONE_DIR)
+		return fmt.Errorf("not found data directory(%s) on this machine, Please make sure to install before upgrade", local.STANDALONE_DIR)
 	}
 	return err
 }
 
-func GetTensorleapCluster(ctx context.Context) (*k3d.Cluster, error) {
+func ValidateClusterExists(ctx context.Context) error {
 	cluster, err := k3d.GetCluster(ctx)
 	if err != nil {
 		log.SendCloudReport("error", "Failed getting k3d cluster", "Failed", &map[string]interface{}{"error": err.Error()})
-		return nil, err
+		return err
 	}
 	if cluster == nil {
 		log.SendCloudReport("error", "K3d cluster found was null", "Failed", &map[string]interface{}{"error": err.Error()})
-		return nil, fmt.Errorf("Not found local Cluster(%s) on this machine, Please make sure to install before upgrade", local.STANDALONE_DIR)
+		return fmt.Errorf("not found local Cluster(%s) on this machine, Please make sure to install before upgrade", local.STANDALONE_DIR)
 	}
-	return cluster, nil
+	return nil
 }
