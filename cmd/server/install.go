a1dafca817836f7a941e9c7a466a044b13dbbbb4
Moshe Kabala <moshe.kabala@tensorleap.ai>
2023-09-04 14:39:02 +0300

    Integrate the installation cmd from helm-charts package

--- cmd/server/install.go
+++ cmd/server/install.go
@@ -1,7 +1,35 @@
 package server
 
-// todo: wrap install command and login after installation
-// apiLink := fmt.Sprintf("%s/api/v2", baseLink)
-// if err := auth.Login("", apiLink); err != nil {
-// 	return err
-// }
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/tensorleap/helm-charts/cmd/server"
+	"github.com/tensorleap/leap-cli/pkg/auth"
+)
+
+func NewInstallCmd() *cobra.Command {
+	flags := &server.InstallFlags{}
+
+	cmd := &cobra.Command{
+		Use:   "install",
+		Short: server.InstallCmdDescription,
+		Long:  server.InstallCmdDescription,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			err := server.RunInstallCmd(cmd, flags)
+			if err != nil {
+				return err
+			}
+			baseLink := fmt.Sprintf("http://127.0.0.1:%v", flags.Port)
+			apiLink := fmt.Sprintf("%s/api/v2", baseLink)
+			if err := auth.Login("", apiLink); err != nil {
+				return err
+			}
+			return nil
+		},
+	}
+
+	server.SetInstallCmdFlags(cmd, flags)
+
+	return cmd
+}
