da3847c1bf4d22535d002e2fc675c19cfdf126b8
Yuval Feldman <yuvalfeldman@tensorleap.ai>
2023-09-11 13:18:24 +0300

    Update import model to wait and show validation errors if any

--- pkg/model/api.go
+++ pkg/model/api.go
@@ -2,17 +2,20 @@ package model
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
+	"github.com/olekukonko/tablewriter"
 	"github.com/tensorleap/leap-cli/pkg/api"
 	"github.com/tensorleap/leap-cli/pkg/code"
 	"github.com/tensorleap/leap-cli/pkg/log"
 	tlApi "github.com/tensorleap/leap-cli/pkg/tensorleapapi"
 )
 
-func ImportModel(ctx context.Context, filePath, projectId, message, modelType, branchName, datasetId string, transformInput bool) error {
+func ImportModel(ctx context.Context, filePath, projectId, message, modelType, branchName, datasetId string, transformInput bool, waitForResults bool) error {
 	fileName := filepath.Base(filePath)
 	versionName := message
 	modelName := strings.TrimSuffix(fileName, filepath.Ext(fileName))
@@ -38,9 +41,11 @@ func ImportModel(ctx context.Context, filePath, projectId, message, modelType, b
 	if len(branchName) > 0 {
 		importModelParams.BranchName = &branchName
 	}
+
+	var mappingYaml string = ""
 	if len(datasetId) > 0 {
 		importModelParams.DatasetId = &datasetId
-		mappingYaml := code.GetDatasetMappingYaml(ctx, datasetId)
+		mappingYaml = code.GetDatasetMappingYaml(ctx, datasetId)
 		importModelParams.MappingYaml = &mappingYaml
 	}
 	if transformInput && modelType == string(tlApi.IMPORTMODELTYPE_ONNX) {
@@ -51,6 +56,97 @@ func ImportModel(ctx context.Context, filePath, projectId, message, modelType, b
 		return err
 	}
 
-	log.Println("Starting import model job. JobId: ", importModelData.GetImportModelJobId())
+	importModelJobId := importModelData.GetImportModelJobId()
+	if waitForResults {
+		okStatus, job, err := waitForImportModelJob(ctx, importModelJobId)
+		if err != nil {
+			return err
+		}
+		if okStatus {
+			log.Println("Successfully imported model")
+			if mappingYaml != "" {
+				printMappingValidationErrors(ctx, job.VersionId, projectId, mappingYaml)
+			}
+		} else {
+			log.Println("Failed to import model")
+		}
+		return nil
+	}
+
+	log.Println("Starting import model job. JobId: ", importModelJobId)
 	return nil
+}
+
+func waitForImportModelJob(ctx context.Context, importModelJobId string) (ok bool, job *tlApi.RunProcess, err error) {
+	message := "Waiting for import model result..."
+	sleepDuration := 3 * time.Second
+	timeoutDuration := 10 * time.Minute
+
+	condition := func() (bool, error) {
+		data, _, err := api.ApiClient.GetTeamJobs(ctx).Execute()
+		if err != nil {
+			return false, fmt.Errorf("failed to wait for the import model job status: %v", err)
+		}
+		if len(data.Processes) == 0 {
+			return false, fmt.Errorf("failed to wait for the import model job status")
+		}
+
+		job = findRunProcessByJobId(data.Processes, importModelJobId)
+		switch job.Status {
+		case tlApi.JOBSTATUS_FAILED:
+			ok = true
+			return true, nil
+		case tlApi.JOBSTATUS_FINISHED:
+			ok = true
+			return true, nil
+		}
+
+		return false, nil
+	}
+
+	err = api.WaitForCondition(ctx, message, condition, sleepDuration, timeoutDuration)
+
+	if err == api.ErrorTimeout {
+		return false, nil, fmt.Errorf("timeout occurred while waiting for import job status")
+	}
+	if err != nil {
+		return false, nil, fmt.Errorf("failed to wait for import job result status: %v", err)
+	}
+
+	return
+}
+
+func findRunProcessByJobId(runProcesses []tlApi.RunProcess, jobId string) *tlApi.RunProcess {
+	for _, rp := range runProcesses {
+		if rp.JobId == jobId {
+			return &rp
+		}
+	}
+	return nil
+}
+
+func printMappingValidationErrors(ctx context.Context, versionId string, projectId string, mapping string) {
+	getValidationErrorsParams := *tlApi.NewCodeIntegrationMappingErrorsParams(projectId, versionId, mapping)
+
+	data, _, err := api.ApiClient.GetCodeIntegrationMappingErrorsByVersionId(ctx).CodeIntegrationMappingErrorsParams(getValidationErrorsParams).Execute()
+	if err != nil {
+		log.Println("failed getting mapping validation errors:", err)
+		return
+	}
+	if data != nil && data.MappingErrors != nil && len(data.MappingErrors) > 0 {
+		log.Println("encountered the following validation errors while applying mapping to graph:")
+
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader([]string{"Title", "Message"})
+		table.SetBorder(true)  // Set Border to false if you don't want borders
+		table.SetRowLine(true) // Enable row line separation
+
+		for _, entry := range data.MappingErrors {
+			table.Append([]string{entry.Title, entry.Message})
+		}
+		table.Render()
+
+	} else {
+		log.Println("mapping was applied successfully applied with no validation errors")
+	}
 }
