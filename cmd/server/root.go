92e7aa35b3360e03833c401b8240cf99901e7e98
Moshe Kabala <moshe.kabala@tensorleap.ai>
2023-09-14 23:02:00 +0300

    Print version

--- cmd/server/root.go
+++ cmd/server/root.go
@@ -1,17 +1,30 @@
 package server
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 	"github.com/tensorleap/helm-charts/cmd/server"
+	"github.com/tensorleap/helm-charts/pkg/version"
 )
+
+var showVersion bool
 
 var RootCommand = &cobra.Command{
 	Use:   "server",
 	Short: "Manage local server installation of Tensorleap",
 	Long:  `Manage local server installation of Tensorleap`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if showVersion {
+			fmt.Println(version.Version)
+		} else {
+			_ = cmd.Help()
+		}
+	},
 }
 
 func init() {
+	RootCommand.PersistentFlags().BoolVarP(&showVersion, "version", "v", false, "Show server installer version")
 	RootCommand.AddCommand(NewInstallCmd())
 	RootCommand.AddCommand(NewUpgradeCmd())
 	RootCommand.AddCommand(NewReinstallCmd())
