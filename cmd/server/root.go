e5172cc335e5b7009660326862a1e64452e4aa40
Moshe Kabala <moshe.kabala@tensorleap.ai>
2024-04-17 14:41:19 +0300

    fixup! fixup! Move save to config

--- cmd/server/root.go
+++ cmd/server/root.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/tensorleap/helm-charts/cmd/server"
+	"github.com/tensorleap/helm-charts/pkg/local"
 	"github.com/tensorleap/helm-charts/pkg/version"
 )
 
@@ -16,7 +17,10 @@ var RootCommand = &cobra.Command{
 	Long:  `Manage local server installation of Tensorleap`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if showInfo {
-			dataDir, _ := getDataDir()
+			dataDir := getConfigureDataDir()
+			if dataDir == "" {
+				dataDir = local.GetServerDataDir()
+			}
 			fmt.Println("Installer Version: ", version.Version)
 			fmt.Println("Data dir:          ", dataDir)
 
@@ -31,8 +35,8 @@ func init() {
 	RootCommand.AddCommand(NewInstallCmd())
 	RootCommand.AddCommand(NewUpgradeCmd())
 	RootCommand.AddCommand(NewReinstallCmd())
+	RootCommand.AddCommand(NewUninstallCmd())
 	RootCommand.AddCommand(server.NewRunCmd())
 	RootCommand.AddCommand(server.NewStopCmd())
-	RootCommand.AddCommand(server.NewUninstallCmd())
 	RootCommand.AddCommand(server.NewToolCmd())
 }
