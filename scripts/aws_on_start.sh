2c508f9bacba0a37dffa2b2867b1d621f41aa87f
Moshe Kabala <moshe.kabala@tensorleap.ai>
2024-08-06 15:26:22 +0300

    Ignore script warning

--- scripts/aws_on_start.sh
+++ scripts/aws_on_start.sh
@@ -2,4 +2,7 @@
 
 set -e
 curl -s https://raw.githubusercontent.com/tensorleap/leap-cli/master/scripts/aws_lifecycle_configurations.sh | sudo bash
+
+# Ignore ShellCheck warning SC1090 for non-constant source path
+# shellcheck source=/dev/null
 source ~/.profile
