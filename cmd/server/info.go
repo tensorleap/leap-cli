e540e6845d089eaa0d40ac7721add4822fd9fcbb
Moshe Kabala <moshe.kabala@tensorleap.ai>
2024-11-25 15:31:47 +0200

    Add info command

---
+++ cmd/server/info.go
@@ -0,0 +1,37 @@
+package server
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/tensorleap/helm-charts/pkg/server"
+	"github.com/tensorleap/leap-cli/pkg/log"
+	"gopkg.in/yaml.v3"
+)
+
+func NewInfoCmd() *cobra.Command {
+
+	cmd := &cobra.Command{
+		Use:   "info",
+		Short: "Server installation information",
+		Long:  "Server installation information",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			installationParams, err := server.LoadInstallationParamsFromPrevious()
+			if err == server.ErrNoInstallationParams {
+				log.Info("No installation information found")
+				return nil
+			}
+			if err != nil {
+				return err
+			}
+
+			log.Info("Installation information:")
+			installationParamsYaml, err := yaml.Marshal(installationParams)
+			if err != nil {
+				return err
+			}
+			log.Info(string(installationParamsYaml))
+			return nil
+		},
+	}
+
+	return cmd
+}
