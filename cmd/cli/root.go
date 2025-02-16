47c5bbb041ff3b80c44e2eeb77be2517f955b699
Moshe Kabala <31233967+moshe-kabala@users.noreply.github.com>
2025-02-16 17:28:57 +0200

    Add cli version (#142)

--- cmd/cli/root.go
+++ cmd/cli/root.go
@@ -1,11 +1,28 @@
 package cli
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
+	"github.com/tensorleap/leap-cli/pkg/version"
 )
+
+var showVersionInfo bool
 
 var RootCommand = &cobra.Command{
 	Use:   "cli",
 	Short: "Manage leap cli",
 	Long:  `Manage leap cli`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if !showVersionInfo {
+			_ = cmd.Help()
+			return
+		}
+
+		fmt.Println(version.CliVersion)
+	},
+}
+
+func init() {
+	RootCommand.Flags().BoolVar(&showVersionInfo, "version", false, "Show version information")
 }
