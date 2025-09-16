4b5ce8deef4539220d1203bd3e2de8532d64fdef
Moshe Kabala <moshe.kabala@tensorleap.ai>
2025-09-16 12:28:38 +0300

    Handle unauthniticated requests

--- pkg/api/utils.go
+++ pkg/api/utils.go
@@ -2,10 +2,12 @@ package api
 
 import (
 	"crypto/tls"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 // HTTPError represents an error with details about an HTTP response.
@@ -15,19 +17,46 @@ type HTTPError struct {
 	Body       string
 }
 
+var ErrAuth = errors.New("user is not currently authenticated, to authenticate - please run 'leap auth login'")
+
 func (e *HTTPError) Error() string {
+	if e.StatusCode == 401 {
+		return ErrAuth.Error()
+	}
 	return fmt.Sprintf("Error: HTTP status code %d\nURL: %s\nResponse body:\n%s", e.StatusCode, e.URL, e.Body)
+}
+
+type CustomRoundTripper struct {
+	originalRoundTripper http.Transport
+}
+
+func (c *CustomRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	thereIsNoDomain := strings.HasPrefix(req.URL.String(), "/")
+	if thereIsNoDomain {
+		return nil, ErrAuth
+	}
+	res, err := c.originalRoundTripper.RoundTrip(req)
+	if err != nil {
+		return res, err
+	}
+	if res.StatusCode == 401 {
+		return res, ErrAuth
+	}
+	return res, nil
 }
 
 // NewDefaultClient returns a new http.Client with default settings.
 func NewDefaultClient() *http.Client {
 	customHttpClient := &http.Client{}
 
+	roundTripper := &CustomRoundTripper{
+		originalRoundTripper: http.Transport{},
+	}
+	customHttpClient.Transport = roundTripper
+
 	if os.Getenv("LEAP_SKIP_SSL_VERIFY") == "true" {
 		// skip SSL verification
-		customHttpClient.Transport = &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		}
+		roundTripper.originalRoundTripper.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	}
 
 	return customHttpClient
@@ -40,6 +69,15 @@ func CheckRes(response *http.Response, err error) error {
 	if IsValidStatus(response) {
 		return nil
 	}
+
+	if response.StatusCode == 401 {
+		return &HTTPError{
+			URL:        response.Request.URL.String(),
+			StatusCode: response.StatusCode,
+			Body:       ErrAuth.Error(),
+		}
+	}
+
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		body = []byte(fmt.Sprintf("Error reading response body: %s", err))
