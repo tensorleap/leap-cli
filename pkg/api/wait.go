71132d740f0519335c36864f1655f1742970efa8
Moshe Kabala <moshe.kabala@tensorleap.ai>
2025-10-29 11:33:39 +0200

    Wait for steps to ended

--- pkg/api/wait.go
+++ pkg/api/wait.go
@@ -37,12 +37,16 @@ func WaitForCondition(ctx context.Context, message string, condition func() (boo
 	return ErrorTimeout
 }
 
+var DELAY_NOT_ENDED_STEP_DURATION = 4 * time.Second
+
 func WaitForConditionWithSteps(ctx context.Context, condition func() (bool, []log.Step, error), sleepDuration time.Duration, timeoutDuration time.Duration) error {
 	startTime := time.Now()
 
 	renderer := log.NewRenderer()
 	renderer.Start()
 	defer renderer.Stop()
+
+	var doneTime *time.Time
 
 	for time.Since(startTime) < timeoutDuration {
 		select {
@@ -55,12 +59,33 @@ func WaitForConditionWithSteps(ctx context.Context, condition func() (bool, []lo
 				return err
 			}
 			if done {
-				time.Sleep(log.FrameDuration)
-				return nil
+				isAllStepsDone := isAllStepsEnded(steps)
+				if isAllStepsDone {
+					time.Sleep(log.FrameDuration)
+					return nil
+				}
+				if doneTime == nil {
+					now := time.Now()
+					doneTime = &now
+				}
+
+				if time.Since(*doneTime) > DELAY_NOT_ENDED_STEP_DURATION {
+					time.Sleep(log.FrameDuration)
+					return nil
+				}
 			}
 			time.Sleep(sleepDuration)
 		}
 	}
 
 	return ErrorTimeout
+}
+
+func isAllStepsEnded(steps []log.Step) bool {
+	for _, step := range steps {
+		if step.Status != log.StepStatusDone && step.Status != log.StepStatusFailed {
+			return false
+		}
+	}
+	return true
 }
