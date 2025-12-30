97249a86115da2afdfe6a03a6b8f00919b2dcc26
Moshe Kabala <moshe.kabala@tensorleap.ai>
2025-12-30 18:54:08 +0200

    Fix server info after re-install

--- cmd/server/info.go
+++ cmd/server/info.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/tensorleap/helm-charts/pkg/k3d"
 	"github.com/tensorleap/helm-charts/pkg/server"
 	"github.com/tensorleap/leap-cli/pkg/log"
 	"gopkg.in/yaml.v3"
@@ -24,6 +25,22 @@ func NewInfoCmd() *cobra.Command {
 			}
 
 			log.Info("Installation information:")
+
+			// Check if cluster exists before trying to get helm chart version
+			cluster, err := k3d.GetCluster(cmd.Context())
+			if err != nil {
+				return err
+			}
+			if cluster == nil {
+				log.Info("Server is not running (cluster not found)")
+				installationParamsYaml, err := yaml.Marshal(installationParams)
+				if err != nil {
+					return err
+				}
+				log.Info(string(installationParamsYaml))
+				return nil
+			}
+
 			currentVersion, err := server.GetCurrentInsalledHelmChartVersion(cmd.Context())
 			if err != nil {
 				return err
